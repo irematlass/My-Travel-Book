5c41e58e07f71324d0c6694d9500b25be1e9eb3e
Mac9 <mac9@Mac9s-iMac.local>
2019-05-02 11:34:07 +0300

    Initial Commit

---
+++ My Travel Book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Travel Book
+//
+//  Created by Mac9 on 2.05.2019.
+//  Copyright © 2019 Mac9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
